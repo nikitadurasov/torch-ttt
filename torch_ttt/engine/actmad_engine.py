c4b4e25cab540b9e57bef6bdc33d8420e3d88351
Nikita Durasov <yassnda@gmail.com>
2024-12-29 23:51:21 +0100

    💄 Update ActMAD structure and methods

--- torch_ttt/engine/actmad_engine.py
+++ torch_ttt/engine/actmad_engine.py
@@ -1,6 +1,8 @@
 import torch
 from typing import List, Dict, Any, Tuple
 from contextlib import contextmanager
+
+from torch.utils.data import DataLoader
 from torch_ttt.engine.base_engine import BaseEngine
 from torch_ttt.engine_registry import EngineRegistry
 
@@ -16,6 +18,39 @@ class ActMADEngine(BaseEngine):
         model (torch.nn.Module): Model to be trained with TTT.
         features_layer_names (List[str]): The names of the layers from which the features are extracted.
         optimization_parameters (dict): The optimization parameters for the engine.
+
+    :Example:
+
+    .. code-block:: python
+
+        from torch_ttt.engine.actmad_engine import ActMADEngine
+
+        model = MyModel()
+        engine = ActMADEngine(model, ["fc1", "fc2"])
+        optimizer = torch.optim.Adam(engine.parameters(), lr=1e-4)
+
+        # Training
+        engine.train()
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            outputs, loss_ttt = engine(inputs)
+            loss = criterion(outputs, labels) + alpha * loss_ttt
+            loss.backward()
+            optimizer.step()
+
+        # Compute statistics for features alignment
+        engine.compute_statistics(train_loader)
+
+        # Inference
+        engine.eval()
+        for inputs, labels in test_loader:
+            output, loss_ttt = engine(inputs)
+
+    Reference:
+
+        "ActMAD: Activation Matching to Align Distributions for Test-Time Training", M. Jehanzeb Mirza, Pol Jane Soneira, Wei Lin, Mateusz Kozinski, Horst Possegger, Horst Bischof
+
+        Paper link: https://openaccess.thecvf.com/content/CVPR2023/papers/Mirza_ActMAD_Activation_Matching_To_Align_Distributions_for_Test-Time-Training_CVPR_2023_paper.pdf
 
     """
 
@@ -50,6 +85,17 @@ class ActMADEngine(BaseEngine):
 
         Returns:
             Returns the current model prediction and rotation loss value.
+        """
+        raise NotImplementedError
+    
+    def compute_statistics(self, dataloader: DataLoader) -> None:
+        """Extract and compute reference statistics for features.
+
+        Args:
+            dataloader (DataLoader): The dataloader used for extracting features. It can return tuples of tensors, with the first element expected to be the input tensor.
+
+        Raises:
+            ValueError: If the dataloader is empty or features have mismatched dimensions.
         """
         raise NotImplementedError
 
