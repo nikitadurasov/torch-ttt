f8c2c6b3cf59f267a5acf2527455277b671fb099
Nikita Durasov <yassnda@gmail.com>
2025-04-19 23:22:38 +0200

    ✨ Add implementation for Masksed Test-Time Training

--- torch_ttt/engine/base_engine.py
+++ torch_ttt/engine/base_engine.py
@@ -4,11 +4,13 @@ import torch.nn as nn
 import torch
 from copy import deepcopy
 
+
 class BaseEngine(nn.Module, ABC):
 
     def __init__(self):
         nn.Module.__init__(self)
         self.optimization_parameters = {}
+        self.optimizer = None
 
     @abstractmethod
     def ttt_forward(self, inputs) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -19,9 +21,9 @@ class BaseEngine(nn.Module, ABC):
         if self.training:
             return self.ttt_forward(inputs)
 
-        # TODO: optimization pipeline should be more flexible and 
+        # TODO: optimization pipeline should be more flexible and
         # user-defined, need some special structure for that
-        optimization_parameters = self.optimization_parameters or {} 
+        optimization_parameters = self.optimization_parameters or {}
 
         optimizer_name = optimization_parameters.get("optimizer_name", "adam")
         num_steps = optimization_parameters.get("num_steps", 3)
@@ -31,17 +33,22 @@ class BaseEngine(nn.Module, ABC):
         running_engine = deepcopy(self) if copy_model else self
 
         parameters = filter(lambda p: p.requires_grad, running_engine.model.parameters())
-        if optimizer_name == "adam":
-            optimizer = torch.optim.Adam(parameters, lr=lr)
+        if copy_model or self.optimizer is None:
+            
+            if optimizer_name == "adam":
+                self.optimizer = torch.optim.Adam(parameters, lr=lr)
 
-        loss = 0 # default value
-        for _ in range(num_steps):
-            optimizer.zero_grad()
+        loss = torch.Tensor([0.0]) # default value
+        for i in range(num_steps):
+            self.optimizer.zero_grad()
             _, loss = running_engine.ttt_forward(inputs)
             loss.backward()
-            optimizer.step()
+            self.optimizer.step()
 
         with torch.no_grad():
-            final_outputs = running_engine.model(inputs)
+            if isinstance(inputs, dict):
+                final_outputs = running_engine.model(**inputs)
+            else:
+                final_outputs = running_engine.model(inputs)
 
         return final_outputs, loss
